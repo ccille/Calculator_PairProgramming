2ec74fb8989dbed7192b352512159977fcc0b40a
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-11-02 12:43:59 +0100

    Vi kan lægge sammen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const App = Vue.createApp({
     data() {
         return {
-
+            firstNumber: 0,
+            secondNumber: 0,
+        }
+    },
+    methods:{
+        Result(){
+            return this.firstNumber + this.secondNumber
         }
     }
 });
